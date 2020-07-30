a231b667a9ca014a6f16aeb04bd1b1839e4ba6f8
Virgil Cantu <cantu.sampsom@gmail.com>
2020-07-30 19:21:06 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,7 @@
 require 'pry'
 
-def prime?(interger)
-if interger / 1 == interger && interger / interger == 1 && interger > 1 && interger != 0
-  true
-else
-  false
+def prime?(n)
+if n <= 1
 
 end
 end
